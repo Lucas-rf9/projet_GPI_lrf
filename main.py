7d4b403f35d6b09c4e8b3424366be3215f244428
Lucas-rf9 <tizioolesoy@gmail.com>
2026-05-19 17:03:52 +0200

    add class

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+#création des différentes classes
+class Atom:
+    def __init__(self,name,coord_x,coord_y,coord_z):
+        self.name = name
+        self.x = coord_x
+        self.y = coord_y
+        self.z = coord_z
 
-class Protein:
-    def __init__(self,name):
-        self.nom = name
-
-    pass 
-
-protein1 = Protein('hemoglobin')
-protein2 = Protein('cytochrom c')
-print(protein2.nom)
+class Nucleotide:
+    def __init__(self,name,position):
+        self.name = name
+        self.position = position
+        self.list_atom = []
+    
+    def add_atoms(self,atoms):
+        self.list_atom.append(atoms)
+ 
